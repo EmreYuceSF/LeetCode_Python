1bbb259feef69ae22a45db29c82270ba8f9407e1
Emre <emreyuce987@icloud.com>
2023-06-21 23:23:19 -0700

    Solved!

---
+++ 125_Valid_Palindrome.py
@@ -0,0 +1,19 @@
+def isPalindrome(s: str) -> bool:
+    """******************************
+    function takes string char by char and if char alpha or num 
+    collects in a list then compare it to reverse of it.
+
+    Args:
+        s (str): string (can have any character and white space)
+
+    Returns:
+        bool: return true if reversed list is equal to original list
+        *****************************
+    """
+    lst = []
+    for a in s:
+        if a.isanum():
+            lst.append(a.lower())
+            
+    return lst==lst[-1: : -1] 
+   
